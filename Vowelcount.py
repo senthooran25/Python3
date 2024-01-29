b4741a7fe078767cba561087b850493bd008168e
senthooran25 <sri.senthooran@gmail.com>
2024-01-29 13:06:43 -0500

    2nd set of files added

---
+++ Vowelcount.py
@@ -0,0 +1,8 @@
+"""Write a function which returns
+the total number of vowels in an input string
+which consists of alphabetic characters.
+Note that capitalization does not matter here
+i.e. a lower case character should be considered
+the same as an upper case character. For this problem,
+the vowels are considered to be A, E, I, O, U."""
+
