9e3c85c3216c4dc7c00ec1577cf71fc2327e78e6
Kumar Gunaretnam <43890754+gSivakumaran@users.noreply.github.com>
2024-01-31 11:32:17 -0500

    Update Celsius2fahenheit.py

--- Celsius2fahenheit.py
+++ Celsius2fahenheit.py
@@ -2,6 +2,7 @@ def celsius2fahrenheit():
     c = float(input("Enter the Temperature Celsius :"))
     f = (c * 9/5) - 32
     print(f"Celsius {c} fahrenheit is {f}")
+    print("kumar change")
 
 
 celsius2fahrenheit()
